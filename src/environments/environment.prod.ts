eb707546c92a49829d3ff75bfebc777030ab6328
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-07-26 00:22:27 +0300

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.github.com/users/',
+  followersUrl: ' https://api.github.com/user/followers',
+  apiRepokey: '/repos?',
 };
